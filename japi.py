8afd7847d032e9d0855b3365f24cc1a180d66900
Tyler Smith <tsmith91@asu.edu>
2019-02-23 21:03:53 -0800

    PD5

---
+++ japi.py
@@ -0,0 +1,22 @@
+import urllib.request
+import json
+
+
+def getStockData(symbol):
+    URL = 'https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=' + symbol + '&apikey=DR0XFIONSG2PIK4A'
+    connection = urllib.request.urlopen(URL)
+    responseString = connection.read().decode()
+    print (responseString)
+    jsonRes = json.loads(responseString)
+    print ("The current price of " + symbol + " is " + jsonRes['Stock Quotes'][0]['2. price'])
+
+
+
+def main():
+    symbol = input('Stock Symbol:')
+    while symbol != 'quit':
+        getStockData(symbol)
+        symbol = input('Stock Symbol:')
+
+
+main()
